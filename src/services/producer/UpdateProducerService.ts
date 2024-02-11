a94bbe3d5416b2cce2bff7df9c1ffa2eb0153051
jefo3 <jeferson.goncalves.noronha@gmail.com>
2024-02-11 06:01:00 -0300

    fix: validating the arable area + vegetation <= total area of ​​the farm and whether the cpf/cnpj is valid

--- src/services/producer/UpdateProducerService.ts
+++ src/services/producer/UpdateProducerService.ts
@@ -5,6 +5,7 @@ import {
   IProducerRepository,
   IUpdate,
 } from "@repositories/IProducersRepository";
+import { isFarmAreasValid, valitadeCpfOrCnpj } from "@shared/utils";
 
 @injectable()
 export class UpdateProducerService {
@@ -14,11 +15,34 @@ export class UpdateProducerService {
   ) {}
 
   async execute({ data, id }: IUpdate) {
+    if (data?.cpfCnpj) valitadeCpfOrCnpj(data.cpfCnpj);
+
     const producerAlreadyExists =
       await this.producersRepository.getProducerById(id);
 
     if (!producerAlreadyExists) {
       throw new Error("Producer Not Exists");
+    }
+
+    const farmArea =
+      data.totalFarmArea || Number(producerAlreadyExists.totalFarmArea);
+
+    const agriculturalArea =
+      data.agriculturalArea || Number(producerAlreadyExists.agriculturalArea);
+
+    const vegetationArea =
+      data.vegetationArea || Number(producerAlreadyExists.vegetationArea);
+
+    const farmAreasIsValid = isFarmAreasValid({
+      farmArea,
+      agriculturalArea,
+      vegetationArea,
+    });
+
+    if (!farmAreasIsValid) {
+      throw new Error(
+        "agriculturalArea next to vegetationArea cannot be greater than the total area of the farm",
+      );
     }
 
     const dataUpdate: UpdateProducerDTO = { ...data, updatedAt: new Date() };
